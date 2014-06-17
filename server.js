fb1eab61a38426f954b29757840f41ccef77849f
Dmitry White <white.dmitry@gmail.com>
2014-06-17 14:16:41 -0700

    Responds to GET request at api/people.  Also included the testing object people with values.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var _ = require('lodash');
 
 app.use(require('morgan')('dev'));
 app.use(require('body-parser')());
@@ -7,7 +8,16 @@ app.use(require('method-override')());
 app.use(express.static(__dirname + '/public'));
 
 
-var people = {};
+var people = {
+  1: { id: 1, name: 'Adam' },
+  2: { id: 2, name: 'Ariel' },
+  3: { id: 3, name: 'Sam' },
+  4: { id: 4, name: 'Grant' }
+};
+
+app.get('/api/people', function (req, res) {
+  res.json({people: _.values(people)});
+});
 
 app.post('/api/people', function(req, res) {
   var id = people.length + 1;
