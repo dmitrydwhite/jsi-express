e93d70cb726aa6f39a465103031abcf5c68e5c6d
J. Allen Tucker <j.allentucker@gmail.com>
2014-06-19 09:23:18 -0700

    created migration for people table

--- server.js
+++ server.js
@@ -2,36 +2,16 @@ var express = require('express');
 var app = express();
 var _ = require('lodash');
 
+var env = process.NODE_ENV || 'development';
+var knexConfig = require('./knexfile.js')[env];
+var knex = require('knex')(knexConfig);
+var bookshelf = require('bookshelf')(knex);
+
 app.use(require('morgan')('dev'));
 app.use(require('body-parser')());
 app.use(require('method-override')());
 app.use(express.static(__dirname + '/public'));
 
-
-var people = {
-  1: { id: 1, name: 'Adam' },
-  2: { id: 2, name: 'Ariel' },
-  3: { id: 3, name: 'Sam' },
-  4: { id: 4, name: 'Grant' }
-};
-
-var idCache = _.keys(people);
-
-var findUniqueKey = function (array) {
-  var nextUnique = 1;
-
-  var iterate = function (array) {
-    if (array.indexOf(nextUnique.toString()) !== -1) {
-      nextUnique += 1;
-      iterate(array);
-    }
-    else {return nextUnique;}
-  };
-
-  iterate(array);
-  idCache.push(nextUnique.toString());
-  return nextUnique;
-};
 
 app.get('/api/people', function (req, res) {
   res.json({people: _.values(people)});
