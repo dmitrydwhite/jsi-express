d25f0b7316db9368ae0d9df5701e5b7cf48b4e51
Dmitry White <white.dmitry@gmail.com>
2014-06-19 13:49:36 -0700

    Deleted unneccessary testing files, added Procfile

--- detectparameterfn.js
+++
@@ -1,8 +0,0 @@
-var detectParameters = function (obj) {
-  var array = _.pairs(obj);
-  var returnObj = {};
-  array.forEach(function (parameterSet) {
-      obj.parameterSet[0] = parameterSet[1];
-  });
-  return obj;
-};
